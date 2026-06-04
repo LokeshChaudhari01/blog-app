2be955c5dac4d0756e2b35a71c74b28ab61c3ceb
Lokesh Chaudhari <lokeshc2705@gmail.com>
2026-06-04 22:37:11 +0530

    updated dependencies

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { signup, login, logout, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/signup', signup);
+router.post('/signup', signup); 
 router.post('/login', login);
 router.post('/logout', logout);
 router.get('/me', protect, getMe);
